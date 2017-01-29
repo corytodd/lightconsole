954f09c45106a54333bdd4f43131c3f472b5ed80
Cory Todd <corneliustodd@gmail.com>
2017-01-28 20:25:59 -0700

    Start work on radiothermostat support

---
+++ RadioThermostat/RadioThermostat.cs
@@ -0,0 +1,37 @@
+﻿using EasyHttp.Http;
+
+namespace RadioThermostat
+{
+    public class RadioThermostat
+    {
+        public RadioThermostat(string host)
+        {
+            Host = host;
+        }
+
+        public string Host { get; private set; }
+
+        public string GetAPIVersion()
+        {
+            return Get(Host, string.Empty);
+        }
+
+        private static string Get(string hostIP, string requestJson)
+        {
+            var http = new HttpClient();
+            http.Request.Accept = HttpContentTypes.ApplicationJson;
+
+            var url = string.Format("http://{0}/tstat", hostIP);
+            try
+            {
+                HttpResponse resp = http.Post(url, requestJson, HttpContentTypes.ApplicationJson);
+                return resp.RawText;
+            }
+            catch (System.Net.WebException e)
+            {
+                return string.Empty;
+            }        
+        }
+
+    }
+}
